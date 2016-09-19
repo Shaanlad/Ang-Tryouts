6ea70efe7ae76e775bdc3e9834937239676f60f6
Shantanu Lad <shaan.lad@gmail.com>
2016-09-19 18:16:14 +0530

    Error.js Controller for Error.html

---
+++ Error.js
@@ -0,0 +1,9 @@
+//Error.js required for Error.html
+angular.module('inputErrorsApp', ['ngMaterial', 'ngMessages'])
+
+.controller('AppCtrl', function($scope) {
+  $scope.project = {
+    description: 'Nuclear Missile Defense System',
+    rate: 500
+  };
+});
